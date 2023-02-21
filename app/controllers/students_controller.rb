df56229465b72bfb6411b79fe517f62be197020f
Nick Scofield <c.nick.scofield@gmail.com>
2023-02-21 15:26:48 -0700

    commit tree 3200ca4e5b91a04d505772d903498bb53ef6159b

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,13 @@
 class StudentsController < ApplicationController
 
+  # Total cheat! README was far from clear on this one, went to the solution code.
   def index
-    students = Student.all
-    render json: students
+    debugger
+    render json: (params[:name]) ? Student.where("first_name LIKE ?", Student.sanitize_sql_like(params[:name]) + "%").or(Student.where("last_name LIKE ?", Student.sanitize_sql_like(params[:name]) + "%")) : Student.all
+  end
+
+  def show
+    render json: Student.find(params[:id]), status: :ok
   end
 
 end
